dd7236d04f98117e28243624ab933e78839bd6ed
Deepali Sharma <65968942+deepali0162@users.noreply.github.com>
2020-10-09 21:46:54 +0530

    Add files via upload

--- SVM.r
+++ SVM.r
@@ -1,6 +1,6 @@
-library(readr)
-library(e1071)
-letterdata <- read_csv("C:/Data Science/letterdata.csv")
+#####Support Vector Machines -------------------
+##  Optical Character Recognition ----
+letterdata<-read.csv("/Users/deepali/Desktop/Data Science/R/letterdata.csv")
 letterdata$letter<-as.factor(letterdata$letter)
 # divide into training and test data
 letters_train <- letterdata[1:16000, ]
@@ -9,7 +9,7 @@ letters_test  <- letterdata[16001:20000, ]
 # begin by training a simple linear SVM
 install.packages("kernlab")
 library(kernlab)
-##Linear method
+
 letter_classifier <- ksvm(letter ~ ., data = letters_train,
                           kernel = "vanilladot")
 View(letter_classifier)
@@ -19,11 +19,14 @@ letter_predictions <- predict(letter_classifier, letters_test)
 head(letter_predictions)
 #table(letter_predictions, letters_test$letter)
 agreement <- letter_predictions == letters_test$letter
-prop.table(table(agreement))
-## Improving model performance ----
+prop.table(table(agreement))  #Accuracy = 83.9%
+## Improving model performance ----non-linear model
 letter_classifier_rbf <- ksvm(letter ~ ., data = letters_train, kernel = "rbfdot")
 letter_predictions_rbf <- predict(letter_classifier_rbf, letters_test)
 head(letter_predictions_rbf)
 agreement_rbf <- letter_predictions_rbf == letters_test$letter
 table(agreement_rbf)
 prop.table(table(agreement_rbf))
+#Accuracy 93%         
+
+           
